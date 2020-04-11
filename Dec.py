caae3a7e7f200e614d6bca9c5613d91e7ba66c60
Kumar Vivek <kumarvivek@kumars-mbp.home>
2020-04-11 12:39:32 +0100

    first version of Dec git status

---
+++ Dec.py
@@ -0,0 +1,35 @@
+a = 10
+e = 30
+b = "abcd"
+c = 2.5
+d = True
+print ("a", a)
+print("e", e)
+print("b", b)
+print("c", c)
+print("d", d)
+print(type(a))
+f = a + e
+print ("f", f)
+f +=1
+print("fnew", f)
+my_list = [25,7,9,13,1]
+my_list.append(7)
+print("third",my_list[2])
+print("last",my_list[4])
+print("new",my_list[5])
+my_list.sort()
+my_list1 = [12,13,14]
+my_list.append(my_list1)
+print(my_list)
+animal = {"cat": "abc","dog": "def","cow": "ghi"}
+print(animal)
+print(animal["cat"])
+print(animal["dog"])
+print(animal["cow"])
+if a == e:
+    print ("a=e")
+elif a > e:
+    print ("a>e")
+else:
+    print ("a<e")
